a4be4c83e40292f1c466d624af493d122b135f30
ZhuravlevAlexei <110524860+ZhuravlevAlexei@users.noreply.github.com>
2023-01-20 12:53:33 +0000

    Deploying to gh-pages from @ ZhuravlevAlexei/goit-js-hw-11@7c3115c3af2eca4be55358f19b9827348226e6af 🚀

--- index.6d86393d.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".search-form").addEventListener("submit",(function(e){e.preventDefault(),console.log(e)}));
-//# sourceMappingURL=index.6d86393d.js.map
--- index.a2637681.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".search-form").addEventListener("submit",(function(e){e.preventDefault(),console.log(e)}));
-//# sourceMappingURL=index.a2637681.js.map
